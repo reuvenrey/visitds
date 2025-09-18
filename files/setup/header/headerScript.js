beab14afb50c5873ba82ded6a6e45061fa20304c
Reuven Reyman <94356206+reuvenrey@users.noreply.github.com>
2025-09-18 10:14:38 -0700

    Update headerScript.js

--- files/setup/header/headerScript.js
+++ files/setup/header/headerScript.js
@@ -1,1 +1,9 @@
+const header = document.getElementById("pgHeader"); // Contents of Header
+const footer = document.getElementById("pgFooter"); // Contents of Footer
+const pgHead = document.getElementById("pgHead"); // Title, meta data, etc.
+const pgLoader = document.getElementById("pgLoader"); // Loading Screen
 
+// Populate contents of page head - necessary for proper display on user-side
+pgHead.innerHTML = '<meta charset="UTF-8">  <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+    <link href="https://fonts.googleapis.com/css?family=FONT_NAME" rel="stylesheet"> \ // load in custom website font
+    <link rel="icon" type="image/x-icon" href=""'; // website icon
