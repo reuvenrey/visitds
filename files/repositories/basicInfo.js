d364528fb205ba6e91ae93bdb3da729a92346cf4
Reuven Reyman <94356206+reuvenrey@users.noreply.github.com>
2026-06-05 12:24:40 -0700

    Create basicInfo.js

---
+++ files/repositories/basicInfo.js
@@ -0,0 +1,24 @@
+specificLocation = ["Downtown", 				 //0
+               	"North District",           // 1
+				"Arts District",            // 2
+				"Entertainment District", 	 // 3
+				"Technology Park", 		 // 4
+				"Media District", 			 // 5
+				"Exposition Park",         // 6
+				"Greater Desert Springs",   // 7
+				"Various Locations"
+               ];
+
+specificCategory = [
+	"Hotels & Resorts",	"Casinos",	"Restaurants",
+	"Attractions & Landmarks",	"Entertainment", "Shopping", "Wellness & Relaxation",
+	"Outdoors & Recreation",	"Culture & Nightlife",	"Family Fun",
+	"Residential",	"Offices", 	"Industrial",
+	"Government Facilities", "Other"
+];
+
+infoCompleteness = ["🚧 Under Construction", "🏢 Exterior Only", "🏡 Exterior, Some Interior", "✅ Fully Complete"];
+	infoCompletenessDetail = ["#FFD900", "#D0FF00", "#6AFF00", "#00BD18"]; // colors of the completeness tag
+
+
+
