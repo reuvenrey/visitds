b65b48296738a95f7b4af771052f637897be467f
Reuven Reyman <94356206+reuvenrey@users.noreply.github.com>
2026-06-09 15:08:29 -0700

    Update basicInfo.js

--- files/repositories/basicInfo.js
+++ files/repositories/basicInfo.js
@@ -4,7 +4,8 @@ specificLocation = ["Downtown", 				 //0
 				"Entertainment District", 	 // 3
 				"Technology Park", 		 // 4
 				"Media District", 			 // 5
-				"Exposition Park",         // 6
+				"Exposition Park",
+				"Highland",		// 6
 				"Greater Desert Springs",   // 7
 				"Various Locations"
                ];
@@ -12,7 +13,7 @@ specificLocation = ["Downtown", 				 //0
 specificCategory = [
 	"Hotels & Resorts",	"Casinos",	"Restaurants",
 	"Attractions & Landmarks",	"Entertainment", "Shopping", "Wellness & Relaxation",
-	"Outdoors & Recreation",	"Culture & Nightlife",	"Family Fun",
+	"Outdoors & Recreation",	"Culture & Nightlife",	"Family Fun", "Event Venue", 
 	"Residential",	"Offices", 	"Industrial",
 	"Government Facilities", "Other"
 ];
