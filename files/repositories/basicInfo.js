61acf3fe6822f31b0126ca9e717ca98a4ba58a9e
Reuven Reyman <94356206+reuvenrey@users.noreply.github.com>
2026-06-15 16:18:28 -0700

    Update basicInfo.js

--- files/repositories/basicInfo.js
+++ files/repositories/basicInfo.js
@@ -11,6 +11,20 @@ specificLocation = ["Downtown", 				 //0
 				"Various Locations"			// 10
                ];
 
+// Description: text || imageLink
+locationDescription = ["The bustling heart of Desert Springs dotted by cloud-surpasing skyscrapers. Downtown is a vibrant and kinetic landscape full of trains, cars, bikes, and business. || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg", 				 //0
+               	"A walkable residential community featuring family-oriented apartments, condos, and town homes and bustling city streets lined with local restaurants, stores, and businesses. || https://i.imgur.com/0F94MPe.png",           // 1
+				"A vibrant neighborhood full of colorful cross-walks, artfully designed residential and business buildings, and walkable streets lively with cheerful commotion. || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg",            // 2
+				"Entertainment District || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg", // 3
+				"Technology Park || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg", 		 // 4
+				"Media District || https://i.imgur.com/UEjz1lO.png", 		// 5
+				"Exposition Park || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg",		// 6
+				"Highland || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg",				// 7
+				"West Valley || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg",			// 8
+				"Greater Desert Springs || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg",   // 9
+				"Various Locations || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg"			// 10
+               ];
+
 specificCategory = [
 	"Hotels & Resorts",	"Casinos",	"Restaurants",
 	"Attractions & Landmarks",	"Entertainment", "Shopping", "Production Studios",
