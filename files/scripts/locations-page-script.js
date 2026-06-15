7d597b49a3ebaca61769843e5b2e0f724605fc21
Reuven Reyman <94356206+reuvenrey@users.noreply.github.com>
2026-06-15 16:33:06 -0700

    Create locations-page-script.js to populate locations page

---
+++ files/scripts/locations-page-script.js
@@ -0,0 +1,34 @@
+const mainDiv = document.getElementById('locationsContainerDiv');
+
+/* from other script:
+specificLocation = location names array
+locationDescription = description || image (indeces should match)
+*/
+
+mainDiv.innerHTML = '';
+
+function setupLocationPage(){
+	for(i=0;i<specificLocation.length;i++){
+		let placeName = specificLocation[i];
+		let [placeDesc, placeImg] = locationDescription[i].split(" || ");
+
+		var placeContent;
+
+		if(i % 2 === 0){
+			// even DIV (img comes first)
+			placeContent = "<img src='"+placeImg+"'><div><h2>"+placeName+"</h2>"+placeDesc+"<p><a class='btn cyan filled noLeft' href='../explore?locations="+i+"'></a></p></div>";
+		}else{
+			// odd DIV (img comes last)
+			placeContent = "<div><h2>"+placeName+"</h2>"+placeDesc+"<p><a class='btn gold filled noLeft' href='../explore?locations="+i+"'></a></p></div><img src='"+placeImg+"'>";
+		}
+
+		mainDiv.innerHTML += "<div class='container'><div class='columnContainer'>"+placeContent+"</div></div>";
+
+		let spot = i + 1;
+		if(spot<specificLocation.length){
+			mainDiv.innerHTML += "<div class='spacer'></div>";
+		}
+	}
+}// end of function setupLocationPage
+
+setupLocationPage();
