6fd10ba2a16411f27e24d81001102a4ad99fca99
Reuven Reyman <94356206+reuvenrey@users.noreply.github.com>
2026-06-17 11:19:50 -0700

    Refined the categories and updated district descriptions

--- files/repositories/basicInfo.js
+++ files/repositories/basicInfo.js
@@ -15,21 +15,21 @@ specificLocation = ["Downtown", 				 //0
 locationDescription = ["The bustling heart of Desert Springs dotted by cloud-surpasing skyscrapers. Downtown is a vibrant and kinetic landscape full of trains, cars, bikes, and business. || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg", 				 //0
                	"A walkable residential community featuring family-oriented apartments, condos, and town homes and bustling city streets lined with local restaurants, stores, and businesses. || https://i.imgur.com/0F94MPe.png",           // 1
 				"A vibrant neighborhood full of colorful cross-walks, artfully designed residential and business buildings, and walkable streets lively with cheerful commotion. || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg",            // 2
-				"Entertainment District || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg", // 3
-				"Technology Park || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg", 		 // 4
-				"Media District || https://i.imgur.com/UEjz1lO.png", 		// 5
+				"Step into a glowing and energetic cityscape of towering casinos, expansive resorts, and miles of entertainment. Stroll down the iconic West Avenue and discover what unlimited fun and infinite possibility awaits! || https://i.imgur.com/OvULKck.png", // 3
+				"Aside from star-studded fame and glamor, Desert Springs also boasts a booming technology industry. Here you'll find some of Minecraftia's most prominent MedTech, Space, Computing, and Engineering Innovators. || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg", 		 // 4
+				"This is where dreams make it to the big screen! The Media District is where creativity, art, and culture come together into brilliant forms for the world to enjoy. || https://i.imgur.com/UEjz1lO.png", 		// 5
 				"Exposition Park || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg",		// 6
 				"Highland || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg",				// 7
 				"West Valley || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg",			// 8
-				"Greater Desert Springs || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg",   // 9
-				"Various Locations || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg"			// 10
+				"Beyond designated districts and neighborhoods exsist small pockets of community and culture to explore. || https://i.imgur.com/A09EWqw.png",   // 9
+				"Various Locations - Ignore! || https://reuvenrey.github.io/visitds/files/images/placeholder.jpg"			// 10
                ];
 
 specificCategory = [
 	"Hotels & Resorts",	"Casinos",	"Restaurants",
 	"Attractions & Landmarks",	"Entertainment", "Shopping", "Production Studios",
-	"Outdoors & Recreation",	"Culture & Nightlife",	"Family Fun", "Event Venue", 
-	"Residential",	"Offices", 	"Industrial",
+	"Outdoors & Recreation", "Museums", "Nightlife", "Cultural", "Family Fun", "Event Venues", 
+	"Residential",	"Offices", 	"Industrial", "Educational Institution", "Healthcare Facility",
 	"Government Facilities", "Other"
 ];
 
